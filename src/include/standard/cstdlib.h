ec83b0da90c731fe89f4f1a080397113f4d15163
Aviad <aviad.bagno@gmail.com>
2021-03-31 16:31:40 +0300

    Created a VERY minimal cstdlib.h

---
+++ src/include/standard/cstdlib.h
@@ -0,0 +1,6 @@
+#ifndef _CSTDLIB_H
+#define _CSTDLIB_H
+
+char* itoa( int value, char * str, int base );
+
+#endif
--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,8 +1,16 @@
 #include <drivers/terminal/terminal.h>
+#include <cstdlib.h>
 
 extern "C" void kernel_main(void) 
 {
 	Terminal t;
 	t.clear();
-	t.print("Hello, Kernel!\n");
+	
+	for (int i = 0; i < 5000; i++)
+	{
+		char str[] = {0, 0, 0, 0, 0};
+		itoa(i, str, 10);
+		t.print(str);
+		t.print("\n");
+	}
 }
