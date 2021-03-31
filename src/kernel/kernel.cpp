2249eb206b6efbf3d257fb96aacf6363a89bb264
Aviad <aviad.bagno@gmail.com>
2021-03-31 17:33:29 +0300

    Removed that stupid loop

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,16 +1,8 @@
 #include <drivers/terminal/terminal.h>
-#include <cstdlib.h>
 
 extern "C" void kernel_main(void) 
 {
 	Terminal t;
 	t.clear();
-	
-	for (int i = 0; i < 5000; i++)
-	{
-		char str[] = {0, 0, 0, 0, 0};
-		itoa(i, str, 10);
-		t.print(str);
-		t.print("\n");
-	}
+	t.print("Hello, Kernel!");
 }
