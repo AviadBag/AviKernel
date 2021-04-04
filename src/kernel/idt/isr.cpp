11098dc6337b462ef32158a74484db6349100d02
Aviad <aviad.bagno@gmail.com>
2021-04-04 19:52:40 +0300

    Added support for all 256 interrupts. (There are still bugs with interrupts above 127)

--- src/kernel/idt/isr.cpp
+++ src/kernel/idt/isr.cpp
@@ -2,20 +2,7 @@
 #include <drivers/terminal/terminal.h>
 #include <cstdlib.h>
 
-extern Terminal terminal;
-
 extern "C" void isr_handler(struct registers regs)
 {
-    terminal.print("Interrupt: number=");
 
-    char s1[] = {0, 0};
-    itoa(regs.interrupt_number, s1, 10);
-    terminal.print(s1);
-
-    terminal.print(", error=");
-    char s2[] = {0, 0};
-    itoa(regs.err_code, s2, 10);
-    terminal.print(s2);
-
-    terminal.print("\n");
 }
