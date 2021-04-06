7b84dd9b7e7c6403c21999271e5f144623ab9d69
Aviad <aviad.bagno@gmail.com>
2021-04-06 16:43:34 +0300

    Added the option to register a custom isr

--- src/include/kernel/idt/isr.h
+++ src/include/kernel/idt/isr.h
@@ -3,13 +3,18 @@
 
 #include <cstdint.h>
 
+#define NUMBER_OF_INTERRUPTS 256
+typedef void (*isr_ptr)(int error_code);
+
 struct registers
 {
-    uint32_t ds;                  // Data segment selector
+    uint32_t ds; // Data segment selector
     uint32_t edi, esi, ebp, useless_value, ebx, edx, ecx, eax; // Pushed by pusha.
-    uint32_t interrupt_number, err_code;    // Interrupt number and error code (if applicable)
+    uint32_t interrupt_number, err_code; // Interrupt number and error code (if applicable)
     uint32_t eip, cs, eflags, esp, ss; // Pushed by the processor automatically.
 } __attribute__((packed));
+
+void register_isr(int interrupt_number, isr_ptr isr);
 
 extern "C" void isr_handler(struct registers regs);
 
--- src/kernel/idt/isr.cpp
+++ src/kernel/idt/isr.cpp
@@ -6,25 +6,36 @@
 #define IRQ_START 32
 #define IRQ_END   47
 
+isr_ptr isrs[NUMBER_OF_INTERRUPTS] = {};
+
 extern Terminal terminal;
 extern "C" void isr_handler(struct registers regs)
 {
+    // Send EOI if this is an IRQ.
     if (regs.interrupt_number >= IRQ_START && regs.interrupt_number <= IRQ_END)
     {
         int irq = regs.interrupt_number - IRQ_START;
         PIC::send_end_of_interrupt(irq);
     }
 
-    terminal.print("Interrupt! number = ");
+//    terminal.print("Interrupt! number = ");
+//
+//    char number[] = {0, 0};
+//    itoa(regs.interrupt_number, number, 10);
+//    terminal.print(number);
+//    terminal.print(", error_code = ");
+//
+//    char error_code[] = {0, 0};
+//    itoa(regs.err_code, error_code, 10);
+//    terminal.print(error_code);
+//
+//    terminal.print("\n");
 
-    char number[] = {0, 0};
-    itoa(regs.interrupt_number, number, 10);
-    terminal.print(number);
-    terminal.print(", error_code = ");
+    // Call the registered handler
+    if (isrs[regs.interrupt_number] != 0) isrs[regs.interrupt_number](regs.err_code);
+}
 
-    char error_code[] = {0, 0};
-    itoa(regs.err_code, error_code, 10);
-    terminal.print(error_code);
-
-    terminal.print("\n");
+void register_isr(int interrupt_number, isr_ptr isr)
+{
+    isrs[interrupt_number] = isr;
 }
