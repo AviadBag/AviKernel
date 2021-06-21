f8554ecc77bcee58d599171fd8a18e960d187547
Aviad Bagno <aviad.bagno@gmail.com>
2021-06-21 10:59:04 +0300

    Changed PageTableEntry to inherit from PagingEntry

--- include/kernel/mm/virtual_mgr/page_table_entry.h
+++ include/kernel/mm/virtual_mgr/page_table_entry.h
@@ -1,32 +1,16 @@
 #include <stdint.h>
 
 #include "kernel/mm/physical_mgr/physical_mgr.h" // For physiacl_addr
+#include "kernel/mm/virtual_mgr/paging_entry.h"
 
 #ifndef _PAGE_TABLE_ENTRY_H
 #define _PAGE_TABLE_ENTRY_H
 
-class PageTableEntry
+class PageTableEntry : public PagingEntry
 {
 public:
-    PageTableEntry(); // By default, all the values are zero.
-    uint32_t to_bytes();
-
-    void set_frame(physical_addr addr); // Must be page aligned!
-    void set_present(bool);
-    void set_writeable(bool);
-    void set_requires_supervisor(bool);
-
-    bool get_present();
-    bool get_writeable();
-    bool get_requires_supervisor();
-    bool get_accessed(); // Access flag
+    PageTableEntry();
     bool get_dirty();  // Dirty flag
-    physical_addr get_frame();
-
-    static PageTableEntry from_bytes(uint32_t);
-
-private:
-    uint32_t entry;
 };
 
 #endif
