019aab7da298b827da317b3742147eea4357dda1
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-02 17:28:53 +0300

    Created virtual_mgr.h very basic definition

---
+++ include/kernel/mm/virtual_mgr/virtual_mgr.h
@@ -0,0 +1,12 @@
+#ifndef __VIRTUAL_MGR_H__
+#define __VIRTUAL_MGR_H__
+
+typedef void* virtual_addr;
+
+class VirtualMgr
+{
+    public:
+        static void initialize();
+};
+
+#endif // __VIRTUAL_MGR_H__
