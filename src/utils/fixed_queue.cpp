fc60e0fa9ffdebf000899713fad336c1d75f3264
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-09 18:34:35 +0300

    Implemented a fixed queue

---
+++ src/utils/fixed_queue.cpp
@@ -0,0 +1,47 @@
+#include "utils/fixed_queue.h"
+
+template class FixedQueue<int>;
+
+template <class T>
+FixedQueue<T>::FixedQueue()
+{
+	head = tail = -1;
+	is_empty = false;
+}
+
+template <class T>
+void FixedQueue<T>::enqueue(T data)
+{
+	is_empty = false; // The queue will be not empty after this enqueue
+
+	tail++;
+	tail %= QUEUE_SIZE;
+	arr[tail] = data;
+
+	if (head == -1)
+		head = 0; // Initialize head
+	if (head == tail) // We have just overriden the first data
+	{
+		head++;
+		head %= QUEUE_SIZE;
+	}
+}
+
+
+template <class T>
+T FixedQueue<T>::dequeue()
+{
+	if (head == tail) // The queue will be empty after this dequeue
+		is_empty = true;
+
+	T data = arr[head];
+	head++;
+	head %= QUEUE_SIZE;
+	return data;
+}
+
+template <class T>
+bool FixedQueue<T>::empty()
+{
+	return is_empty;
+}
