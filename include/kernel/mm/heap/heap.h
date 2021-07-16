46f27d68861b4ba51d88c4cd8a4eee06ff3c39a6
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-16 11:43:00 +0300

    Created heap_header struct

--- include/kernel/mm/heap/heap.h
+++ include/kernel/mm/heap/heap.h
@@ -2,6 +2,14 @@
 #define __HEAP_H__
 
 #include <stddef.h>
+#include <stdint.h>
+
+typedef struct heap_header_e
+{
+    uint32_t size;
+    struct heap_header_e* next;
+    struct heap_header_e* prev;
+} heap_header;
 
 class Heap
 {
