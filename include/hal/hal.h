1670ac67b78dd55899d739fb530349d0deeaa282
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-29 20:37:48 +0300

    HAL: Implemented get_driver().

--- include/hal/hal.h
+++ include/hal/hal.h
@@ -3,11 +3,13 @@
 
 #include "hal/drivers/driver.h"
 
-enum HAL_DRIVER_TYPE
+enum HAL_GENERAL_DRIVER_TYPE
 {
     HAL_KEYBOARD_DRIVER,
     HAL_SCREEN_TEXT_DRIVER
 };
+
+#define HAL_NUMBER_OF_GENERAL_DRIVERS 2
 
 class HAL
 {
@@ -15,13 +17,16 @@ public:
     static HAL* get_instance();
 
     void initialize();
-    Driver* get_driver(HAL_DRIVER_TYPE); // Returns nullptr if the given device does not exist
+    Driver* get_driver(HAL_GENERAL_DRIVER_TYPE); // Returns nullptr if the given device does not exist
 
     HAL(HAL &other) = delete; // Should not be copied
     void operator=(const HAL &) = delete; // Should not be assigned
 
 private:
     HAL(); // It's a singleton; It's constructor should be private.
+
+    // I make it Driver* and not Driver - because I need polymorphism here. Aviad, just think about it a few seconds..
+    Driver* general_drivers[HAL_NUMBER_OF_GENERAL_DRIVERS];
 
     static HAL* instance;
 };
--- src/hal/hal.cpp
+++ src/hal/hal.cpp
@@ -21,9 +21,13 @@ HAL* HAL::get_instance()
 void HAL::initialize() 
 {
     // This is where the fun begins!
+    
+    // Fill general_drivers[] with nullptr's, so if a device does not exist - get_driver() will return nullptr.
+    for (int i = 0; i < HAL_NUMBER_OF_GENERAL_DRIVERS; i++)
+        general_drivers[i] = nullptr;
 }
 
-Driver* HAL::get_driver(HAL_DRIVER_TYPE driver_type) 
+Driver* HAL::get_driver(HAL_GENERAL_DRIVER_TYPE driver_type) 
 {
-    return nullptr;
+    return general_drivers[driver_type];
 }
