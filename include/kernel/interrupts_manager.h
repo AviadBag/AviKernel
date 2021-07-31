92c05d1fc304c195c662809f2e4356a88ff791fb
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-31 21:08:25 +0300

    Created file interrupts_manager.h

---
+++ include/kernel/interrupts_manager.h
@@ -0,0 +1,23 @@
+#ifndef __InterruptsManager_MANAGER_H__
+#define __InterruptsManager_MANAGER_H__
+
+// This module manages all of the InterruptsManager stuff. This is a singelton!
+
+class InterruptsManager
+{
+public:
+    void initialize();
+
+/* ------------------------- Singelton stuff. ------------------------- */
+public:
+    static InterruptsManager* get_instance();
+
+    InterruptsManager(InterruptsManager& other) = delete; // Should not be cloneable.
+    void operator=(const InterruptsManager& other) = delete;
+private:
+    InterruptsManager(); // This is a singelton - So a private constructor.
+
+    static InterruptsManager* instance;
+};
+
+#endif // __InterruptsManager_MANAGER_H__
