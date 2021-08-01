ac40f0edac9e9902e0080f880698757339909a4d
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-01 14:23:20 +0300

    driver.h: Pointed out that attach must be called only if exist returns true

--- include/hal/drivers/driver.h
+++ include/hal/drivers/driver.h
@@ -4,7 +4,7 @@
 class Driver
 {
 public:
-    virtual void attach() = 0;
+    virtual void attach() = 0; // Undefined results if called when exist() returns false.
     virtual void detach() = 0;
     virtual bool exist() = 0; // Does this device/s exist? CAN BE CALLED BEFORE attach()!
 };
