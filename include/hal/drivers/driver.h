5eedeef1b0ef389011e43f4dd0cfe8a40de8b3fa
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-01 14:24:14 +0300

    driver.h: Pointed out that detach must be called after attach.

--- include/hal/drivers/driver.h
+++ include/hal/drivers/driver.h
@@ -5,8 +5,8 @@ class Driver
 {
 public:
     virtual void attach() = 0; // Undefined results if called when exist() returns false.
-    virtual void detach() = 0;
-    virtual bool exist() = 0; // Does this device/s exist? CAN BE CALLED BEFORE attach()!
+    virtual void detach() = 0; // Undefined results if attach was not called before.
+    virtual bool exist() = 0;  // Does this device/s exist? CAN BE CALLED BEFORE attach()!
 };
 
 #endif // __DRIVER_H__
