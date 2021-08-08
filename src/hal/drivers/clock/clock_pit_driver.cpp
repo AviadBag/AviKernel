d4c00b935b85d8ea26e1c64bd3b465a73f08991d
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-08 09:38:39 +0300

    PIT driver: Panic if the frequency does not fit into 16 bits.

--- src/hal/drivers/clock/clock_pit_driver.cpp
+++ src/hal/drivers/clock/clock_pit_driver.cpp
@@ -1,6 +1,7 @@
 #include "hal/drivers/clock/clock_pit_driver.h"
 #include "kernel/interrupts/interrupts_manager.h"
 #include "drivers/serial_ports/serial_ports.h"
+#include "kernel/panic.h"
 
 #define PIT_DIVIDEND 1193180
 
@@ -8,6 +9,8 @@
 #define PIT_CHANNEL_0_PORT 0x40
 
 #define PIT_IRQ_NUMBER 32
+
+#define UINT16_T_MAX 65535 // The maximum number that can fit into uint16_t
 
 void ClockPITDriver::attach() 
 {
@@ -29,6 +32,8 @@ void ClockPITDriver::configure_pit()
 {
     // First, program the PIT to the desired frequency
     uint32_t divisor = PIT_DIVIDEND / frequency;
+    if (divisor > UINT16_T_MAX)
+        panic("Cannot initial PIT with frequency of %d, because the maximum is %d (16 bits unsigned)", divisor, UINT16_T_MAX);
 
     // send the command byte
     SerialPorts::outb(PIT_COMMAND_PORT, 0x36);
