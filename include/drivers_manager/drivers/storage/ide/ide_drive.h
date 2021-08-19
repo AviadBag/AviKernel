de4b36ebae30f54fbae9b0c961e94f563d8f83c5
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-19 20:53:47 +0300

    IDEDrive: Added fields supports_lba and uses_48_bits_mode

--- include/drivers_manager/drivers/storage/ide/ide_drive.h
+++ include/drivers_manager/drivers/storage/ide/ide_drive.h
@@ -6,14 +6,18 @@
 class IDEDrive : public Drive
 {
 public:
-    IDEDrive(uint8_t channel, uint8_t drive_in_channel, uint32_t sector_size, uint64_t number_of_sectors);
+    IDEDrive(uint8_t channel, uint8_t drive_in_channel, bool supports_lba, bool uses_48_bits_mode, uint32_t sector_size, uint64_t number_of_sectors);
 
-    uint8_t get_channel();
-    uint8_t get_drive_in_channel();
+    uint8_t get_channel            ();
+    uint8_t get_drive_in_channel   ();
+    bool    get_supports_lba       ();
+    bool    get_uses_48_bits_mode  ();
 
 private:
     uint8_t channel;
     uint8_t drive_in_channel;
+    bool    supports_lba;
+    bool    uses_48_bits_mode;
 };
 
 #endif // __IDEDRIVE_H__
--- src/drivers_manager/drivers/storage/ide/ide_drive.cpp
+++ src/drivers_manager/drivers/storage/ide/ide_drive.cpp
@@ -1,18 +1,36 @@
 #include "drivers_manager/drivers/storage/ide/ide_drive.h"
 
-IDEDrive::IDEDrive(uint8_t channel, uint8_t drive_in_channel, uint32_t sector_size, uint64_t number_of_sectors)
+IDEDrive::IDEDrive(
+    uint8_t channel,
+    uint8_t drive_in_channel,
+    bool supports_lba,
+    bool uses_48_bits_mode,
+    uint32_t sector_size,
+    uint64_t number_of_sectors)
     : Drive(sector_size, number_of_sectors)
 {
     this->channel = channel;
     this->drive_in_channel = drive_in_channel;
+    this->supports_lba = supports_lba;
+    this->uses_48_bits_mode = uses_48_bits_mode;
 }
 
-uint8_t IDEDrive::get_drive_in_channel() 
+uint8_t IDEDrive::get_drive_in_channel()
 {
     return drive_in_channel;
 }
 
-uint8_t IDEDrive::get_channel() 
+uint8_t IDEDrive::get_channel()
 {
     return channel;
+}
+
+bool IDEDrive::get_supports_lba() 
+{
+    return supports_lba;
+}
+
+bool IDEDrive::get_uses_48_bits_mode() 
+{
+    return uses_48_bits_mode;
 }
