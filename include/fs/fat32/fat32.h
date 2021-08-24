9fd424bbb2a9e141674880c88a7c5491d266bbb6
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-24 14:03:37 +0300

    fat32 driver: reading the boot sector into a boot_sector variable

--- include/fs/fat32/fat32.h
+++ include/fs/fat32/fat32.h
@@ -2,18 +2,60 @@
 #define _FAT32_H
 
 #include "fs/fs.h"
+#include "drivers_manager/drivers/storage/storage_driver.h"
+
+#include <stdint.h>
 
 // This is an implementation of the FAT 32 file system
+
+
+struct fat32_boot_sector
+{
+	uint8_t  unused1[3];
+	char     oem_name[8];
+	uint16_t bytes_per_sector;
+	uint8_t  sectors_per_cluster;
+	uint16_t reserved_sectors_count;
+	uint8_t  number_of_fats;
+	uint16_t unused2;
+	uint16_t unused3;
+	uint8_t  media;
+	uint16_t unused4;
+	uint16_t sectors_per_track;
+	uint16_t number_of_heads;
+	uint32_t number_of_hidden_sectors;
+	uint32_t number_of_sectors;
+	
+	uint32_t sectors_per_fat;
+	uint16_t extra_flags;
+	uint16_t fs_version;
+	uint32_t root_cluster;
+	uint16_t fs_info_sector;
+	uint16_t boot_sector_copy_sector;
+	uint8_t  reserved1[12];
+	uint8_t  unused5;
+	uint8_t  reserved2;
+	uint8_t  boot_sig;
+	uint32_t volume_id;
+	char     volume_label[11];
+	char     fs_type[8];
+	char     boot_code[420];
+	uint16_t bootable_signature;
+} __attribute__((__packed__));
 
 class FAT32 : public FS
 {
 public:
-	virtual void mount(int what) override; 
+	virtual void mount(int drive) override; 
 	virtual void umount() override;
 
 	virtual void read  (char* path, int count, char* buf) override;
 	virtual void write (char* path, int count, char* buf) override;
 	virtual void append(char* path, int count, char* buf) override;
+
+private:
+	fat32_boot_sector boot_sector;
+	StorageDriver* storage_driver;
 };
 
 #endif
--- src/fs/fat32/fat32.cpp
+++ src/fs/fat32/fat32.cpp
@@ -1,46 +1,17 @@
 #include "fs/fat32/fat32.h"
+#include "drivers_manager/drivers_manager.h"
+#include "drivers_manager/drivers/storage/storage_driver.h"
 
-#include <stdint.h>
+#include <cstdio.h>
 
-// Struct definitions
-struct fat32_boot_sector
+void FAT32::mount(int drive) 
 {
-	uint8_t  unused1[3];
-	char     oem_name[8];
-	uint16_t bytes_per_sector;
-	uint8_t  sectors_per_cluster;
-	uint16_t reserved_sectors_count;
-	uint8_t  number_of_fats;
-	uint16_t unused2;
-	uint16_t unused3;
-	uint8_t  media;
-	uint16_t unused4;
-	uint16_t sectors_per_track;
-	uint16_t number_of_heads;
-	uint32_t number_of_hidden_sectors;
-	uint32_t number_of_sectors;
-	
-	uint32_t sectors_per_fat;
-	uint16_t extra_flags;
-	uint16_t fs_version;
-	uint32_t root_cluster;
-	uint16_t fs_info_sector;
-	uint16_t boot_sector_copy_sector;
-	uint8_t  reserved1[12];
-	uint8_t  unused5;
-	uint8_t  reserved2;
-	uint8_t  boot_sig;
-	uint32_t volume_id;
-	char     volume_label[11];
-	char     fs_type[8];
-	char     boot_code[420];
-	uint16_t bootable_signature;
-} __attribute__((__packed__));
-
-
-void FAT32::mount(int what) 
-{
-
+    // Get the storaage driver
+    storage_driver = (StorageDriver*) DriversManager::get_instance()->get_driver(DRIVERS_MANAGER_STORAGE_DRIVER);
+    
+    // Read the boot sector into my structure
+    storage_driver->select_drive(drive);
+    storage_driver->read_sectors(0, 1, (char*) &boot_sector);
 }
 
 void FAT32::umount() 
@@ -48,17 +19,17 @@ void FAT32::umount()
 
 }
 
-void FAT32::read(char* path, int count, char* buf) 
+void FAT32::read([[gnu::unused]] char* path, [[gnu::unused]] int count, [[gnu::unused]] char* buf) 
 {
 
 }
 
-void FAT32::write(char* path, int count, char* buf) 
+void FAT32::write([[gnu::unused]] char* path, [[gnu::unused]] int count, [[gnu::unused]] char* buf) 
 {
 
 }
 
-void FAT32::append(char* path, int count, char* buf) 
+void FAT32::append([[gnu::unused]] char* path, [[gnu::unused]] int count, [[gnu::unused]] char* buf) 
 {
 
 }
