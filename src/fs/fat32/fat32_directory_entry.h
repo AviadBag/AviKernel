c5023e32b295406284c172ba7de09c03b1191eed
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-25 11:05:32 +0300

    fat32 directory entry - fixed some mistakes, did some improvements

--- src/fs/fat32/fat32_directory_entry.h
+++ src/fs/fat32/fat32_directory_entry.h
@@ -8,15 +8,18 @@
 // Methods starting with 'le' are methods regarding only long entries. (is_long_entry() is true).
 // Methods starting with 'se' are methods regarding only short entries. (is_long_entry() is false).
 
+#define FAT32_DIR_ENTRY_LENGTH 32
+
 class FAT32DirectoryEntry
 {
 public:
     // Gets the bytes themselves.
-    FAT32DirectoryEntry(uint32_t bytes);
+    FAT32DirectoryEntry(uint8_t* bytes); // Given an array of 32 bytes. Copies it to an inner array.
 
     // ---------------------- Getters ----------------------
-    bool is_used(); // If false - this does not represent a file.
-    bool is_last(); // If true - it is the last entry of this directory, AND it is not used.
+    bool is_used(); // If true - you surely should use it.
+    bool is_free(); // If true - this does not represent a file.
+    bool is_end(); // If true - it is the last entry of this directory, AND it is not used.
     bool is_directory();
     bool is_archive();
     bool is_long_entry();
@@ -39,10 +42,12 @@ public:
     bool le_is_last();      // Is it the last long entry?
     String le_get_name();
 
-    uint32_t get_bytes();
+    uint8_t* get_bytes(); // Returns the inner bytes array. (Not the given array in the constructor).
 
 private:
-    uint32_t bytes; // The internal representation of the bytes themselves.
+    uint8_t get_attributes();
+
+    uint8_t bytes[FAT32_DIR_ENTRY_LENGTH]; // The internal representation of the bytes themselves.
 };
 
 #endif // __FAT32_DIRECTORY_ENTRY_H__
