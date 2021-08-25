6fc24e243dea631956e5d35d5af7959a84a88261
Aviad Bagno <aviad.bagno@gmail.com>
2021-08-25 10:29:59 +0300

    Vector class - added a sort method

--- include/utils/vector.h
+++ include/utils/vector.h
@@ -23,8 +23,16 @@ public:
     int size() const;
     void append(T); // Panics if there is no enough memory
     T get(int index); // Panic if index is out of range
+    
+    // Sorts the vector, according to the given function.
+    // The given function gets two variables of type T as a parameter. Returns true if the first parameter
+    // is bigger than the second.
+    void sort(bool (*compare)(T first, T second));
 
 private:
+    VectorNode<T>* get_node(int index);
+    void append_node(VectorNode<T>* new_node);
+
     VectorNode<T>* head;
     int vector_size;
 };
@@ -75,6 +83,12 @@ void Vector<T>::append(T data)
     new_node->data = data;
     new_node->next = nullptr;
 
+    append_node(new_node);
+}
+
+template<class T>
+void Vector<T>::append_node(VectorNode<T>* new_node) 
+{
     if (!head)
         head = new_node;
     else {
@@ -91,6 +105,12 @@ void Vector<T>::append(T data)
 template <class T>
 T Vector<T>::get(int index)
 {
+    return get_node(index)->data;
+}
+
+template<class T>
+VectorNode<T>* Vector<T>::get_node(int index) 
+{
     if (index + 1 > size())
         panic("Vector: Index \"%d\" is out of range", index);
 
@@ -98,7 +118,58 @@ T Vector<T>::get(int index)
     while (index--)
         node = node->next;
 
-    return node->data;
+    return node;
+}
+
+template<class T>
+void Vector<T>::sort(bool (*compare)(T first, T second)) 
+{
+    // Insertion sort
+    
+    VectorNode<T>* iterator = head;
+    VectorNode<T>* iterator_prev = nullptr;
+    VectorNode<T>* new_iterator;     // The iterator might move; we want to backup is next.
+    VectorNode<T>* new_iterator_prev; // The iterator might move; We want to save his new prev.
+    int sorted_area_size = 0;
+    
+    while (iterator)
+    {
+        new_iterator = iterator->next;
+        new_iterator_prev = iterator;
+
+        VectorNode<T>* insertion_ptr = head;
+        VectorNode<T>* insertion_ptr_prev = nullptr;
+        for (int i = 0; i < sorted_area_size; i++)
+        {
+            if (compare(insertion_ptr->data, iterator->data))
+            {
+                // We should insert <iterator> behind <insertion_ptr>
+                if (!iterator_prev)
+                    continue; // This is the first item; There is nothing to do.
+
+                new_iterator_prev = iterator_prev; // The operator will move.
+                new_iterator = iterator->next;
+
+                iterator_prev->next = iterator->next; // Now <iterator> is not connected to the list anymore
+                iterator->next = insertion_ptr; // Now iterator is right behind <insertion_ptr>
+                if (!insertion_ptr_prev)
+                    // We are inserting behind the FIRST item - so we should only update <head>
+                    head = iterator;
+                else
+                    // We have to update <insertion_ptr_prev>
+                    insertion_ptr_prev->next = iterator;
+
+                break; // We inserted our var in the right place; no need further investigation.
+            }
+
+            insertion_ptr_prev = insertion_ptr;
+            insertion_ptr = insertion_ptr->next;
+        }
+
+        sorted_area_size++;
+        iterator_prev = new_iterator_prev;
+        iterator = new_iterator;
+    }
 }
 
 #endif // __VECTOR_H__
