7723c14ddf0afd6513273daf8962e651773fa408
Aviad Bagno <aviad.bagno@gmail.com>
2021-09-03 15:28:31 +0300

    Made the path class header file nicer

--- include/fs/path.h
+++ include/fs/path.h
@@ -10,12 +10,15 @@
 class Path
 {
 public:
+    // ------------------- Constructors and Destructor -------------------
     Path(String);
 
-    // The directory depth of this path. Root directory is depth 0. For example - for /a/b/c.txt - depth=2.
-    // And for /a/ - depth=1.
-    int get_depth();
+    // ------------------- Regular Methods -------------------
+    bool is_folder();
+    bool is_legal();
+    void go_up();               // Goes one level up; Must only be called if <folder> = true and <depth> != 0. Panics else.
 
+    // ------------------- Methods with long docs -------------------
     /**
      * Returns the required part, starting from part <0>. Every string inside of the path is a part.
      * For example - /a/b/c/d.txt:
@@ -26,17 +29,21 @@ public:
      * I must be in range; panics if it is not.
      */
     String get_part(int i);
-    
-    bool is_folder();
-    bool is_legal();
-    void fill_vector(String s); // Fills the <parts> vector with the parts of the given string; <legal> must be true.
-    void go_up(); // Goes one level up; Must only be called if <folder> = true and <depth> != 0. Panics else.
+
+    /**
+     * The directory depth of this path. Root directory is depth 0. For example - for /a/b/c.txt - depth=2.
+     * And for /a/ - depth=1.
+    */
+    int get_depth();
 
 private:
+    // ------------------- Regular Methods -------------------
     static bool is_legal(String); // Checks if the GIVEN STRING is legal.
+    void fill_vector(String s);   // Fills the <parts> vector with the parts of the given string; <legal> must be true.
 
-    bool legal = true;
-    bool folder; // If false - it is a file
+    // ------------------- Member Variables -------------------
+    bool legal = true;    // Is this path legal?
+    bool folder;          // If false - it is a file
     Vector<String> parts; // Each slesh seperated portion is a "part".
 };
 
