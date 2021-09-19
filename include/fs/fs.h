9a9bc0afd18130c74f80e411109394156303e851
Aviad Bagno <aviad.bagno@gmail.com>
2021-09-19 15:25:00 +0300

    Changed most of the methods of the FS class to return a status code (fs_status_code)

--- include/fs/fs.h
+++ include/fs/fs.h
@@ -6,6 +6,17 @@
 
 #include <stdint.h>
 
+/**
+ * This enum represents the various status codes that can be returned from a fs operation.
+ */
+enum fs_status_code
+{
+    FS_OK,                   /* Nothing exceptional happend, everything worked */
+    FS_NO_SUCH_FILE,
+    FS_NO_SUCH_DIR,
+    FS_UNSUPPORTED_OPERATION /* For example - you cannot create a directory in devfs */
+};
+
 class FS {
 public:
     virtual ~FS() {};
@@ -14,12 +25,12 @@ public:
     virtual void mount(int what) = 0;
     virtual void umount() = 0;
 
-    virtual void read(Path path, size_t count, size_t offset, char* buf) = 0;
-    virtual void write(Path path, size_t count, size_t offset, char* buf) = 0;
+    virtual fs_status_code read(Path path, size_t count, size_t offset, char* buf) = 0;
+    virtual fs_status_code write(Path path, size_t count, size_t offset, char* buf) = 0;
 
-    virtual void create_file(Path path) = 0;
-    virtual void delete_file(Path path) = 0;
-    virtual Vector<Path> list_files(Path path) = 0;
+    virtual fs_status_code create_file(Path path) = 0;
+    virtual fs_status_code delete_file(Path path) = 0;
+    virtual fs_status_code list_files(Path path, Vector<Path>*) = 0;
 };
 
 #endif
