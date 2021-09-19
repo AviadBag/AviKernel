a9d64d47c2c869326cfd66b6f58d1a64a8bcd9bc
Aviad Bagno <aviad.bagno@gmail.com>
2021-09-19 16:02:22 +0300

    cctype.h - added function isspace

--- include/libc/cctype.h
+++ include/libc/cctype.h
@@ -5,5 +5,6 @@
 #define isupper(c) ((c) >= 'A' && (c) <= 'Z')
 #define tolower(c) (isupper(c) ? (c) + 'a' - 'A' : (c))
 #define toupper(c) (islower(c) ? (c) + 'A' - 'a' : (c))
+#define isspace(c) (c == ' ' || c == '\n' || c == '\t' || c == '\v' || c == '\f' || c == '\r' ? 1 : 0)
 
 #endif
