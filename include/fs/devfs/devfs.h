e8de7107ffd4ff875930673835075af7508a3bfa
Aviad Bagno <aviad.bagno@gmail.com>
2021-09-20 10:59:34 +0300

    Devfs: Implemented reading <sd*>, only when total sectors count is lower than 256.

--- include/fs/devfs/devfs.h
+++ include/fs/devfs/devfs.h
@@ -7,6 +7,9 @@
 
 #include <stdint.h>
 
+/* Used for io() function. (See it's docs) */
+enum devfs_operation { DEVFS_OPR_READ, DEVFS_OPR_WRITE };
+
 class DevFS : public FS
 {
 public:
@@ -22,6 +25,23 @@ public:
     virtual fs_status_code list_files(Path path, Vector<Path>*) override;
 
 private:
+    // ------------------- Regular Methods -------------------
+    fs_status_code storage_drive_read(Path path, size_t count, size_t offset, char* buf); // <path> must be a legal storage drive path. Unexpected results if else!
+
+    // ------------------- Methods with Long Docs -------------------
+    /**
+     * This is the main method of the devfs module. It gets the required operation (r/w), and calls the
+     * appropriate function.
+     * 
+     * @param operation What operation to do? (read/write)
+     * @param path      The path to the file
+     * @param count     How many bytes to read/write?
+     * @param offset    From what offset to begin?
+     * @param buf       The buffer to write from / read into
+     * @return fs_status_code 
+     */
+    fs_status_code io(devfs_operation operation, Path path, size_t count, size_t offset, char* buf);
+
     // ------------------- Member Variables -------------------
     Vector<Path> root_dir; // Will contain /sda, /sdb...
 };
--- src/fs/devfs/devfs.cpp
+++ src/fs/devfs/devfs.cpp
@@ -4,6 +4,7 @@
 #include "drivers_manager/drivers/storage/physical_drive.h"
 
 #include <cstdio.h>
+#include <cstring.h>
 
 void DevFS::mount([[gnu::unused]] int what) 
 {
@@ -27,16 +28,76 @@ void DevFS::umount() {} // Nothing here
 
 fs_status_code DevFS::read(Path path, size_t count, size_t offset, char* buf) 
 {
-    // Does this file exist?
-    if (!root_dir.exist(path))
-        return FS_NO_SUCH_FILE;
-
-    return FS_OK;
+    return io(DEVFS_OPR_READ, path, count, offset, buf);
 }
 
 fs_status_code DevFS::write(Path path, size_t count, size_t offset, char* buf) 
 {
+    return io(DEVFS_OPR_WRITE, path, count, offset, buf);
+}
 
+fs_status_code DevFS::io(devfs_operation operation, Path path, size_t count, size_t offset, char* buf) 
+{
+    // Does this file exist?
+    if (!root_dir.exist(path))
+        return FS_NO_SUCH_FILE;
+
+    // Is it a storage drive?
+    if (path.to_string().substr(1, 2) == "sd" && path.to_string().size() == 4)
+    {
+        if (operation == DEVFS_OPR_READ) return storage_drive_read(path, count, offset, buf);
+        else return FS_UNSUPPORTED_OPERATION;
+    }
+
+    return FS_OK;
+}
+
+fs_status_code DevFS::storage_drive_read(Path path, size_t count, size_t offset, char* buf) 
+{
+    char drive_char = path.to_string()[3]; // "/sda" => 'a', "/sdz" => 'z'...
+    char drive_number = drive_char - 'a';  // 'a' => 0, 'z' => 25...
+
+    /* <count> is prhobably not sector aligned, so we have to write everything
+       to a temporarily buffer with sector aligned size, and then copy just how much we need to the real buffer */
+
+    // Gain the storage driver
+    StorageDriver* storage_driver = (StorageDriver*) DriversManager::get_instance()->get_driver(DRIVERS_MANAGER_STORAGE_DRIVER);
+
+    // Determine sector size
+    uint32_t sector_size = storage_driver->get_drive(drive_number)->get_sector_size();
+
+    // What's the LBA?
+    uint64_t lba = offset / sector_size;
+
+    // How many sectors do we have to read? (Round up)
+    uint64_t starting_sector = lba;
+    uint64_t ending_sector = (offset + count) / sector_size;
+    uint64_t sectors_count = ending_sector - starting_sector + 1;
+
+    // Allocate the temporarily buffer
+    char* temp_buf = new char[sectors_count * sector_size];
+    if (!temp_buf)
+        return FS_NOT_ENOUGH_MEMORY;
+
+    // READ!
+
+    // We can only read up to 255 sectors at a time
+    if (sectors_count <= 255)
+    {
+        // Well, just read...
+        storage_driver->read_sectors(drive_number, lba, sectors_count, temp_buf);
+    }
+    else
+    {
+        delete temp_buf;
+        return FS_UNSUPPORTED_OPERATION;
+    }
+    
+    // Copy to the real buffer
+    memcpy(buf, temp_buf + (offset % sector_size), count);
+    delete temp_buf;
+
+    return FS_OK;
 }
 
 fs_status_code DevFS::create_file([[gnu::unused]] Path path) 
