fd26e6dfd88d668a2ac14f9b06363a6dacb219b4
Aviad Bagno <aviad.bagno@gmail.com>
2022-02-12 22:14:25 +0200

    fs interface - using errno to indicate status instead of fs_status_code

--- include/fs/fs.h
+++ include/fs/fs.h
@@ -6,19 +6,6 @@
 
 #include <stdint.h>
 
-/**
- * This enum represents the various status codes that can be returned from a fs operation.
- */
-enum fs_status_code
-{
-    FS_OK, /* Nothing exceptional happend, everything worked */
-    FS_NO_SUCH_FILE,
-    FS_NO_SUCH_DIR,
-    FS_FILE_IS_REQUIRED,
-    FS_UNSUPPORTED_OPERATION, /* For example - you cannot create a directory in devfs */
-    FS_NOT_ENOUGH_MEMORY
-};
-
 class FS
 {
 public:
@@ -28,13 +15,13 @@ public:
     virtual void mount(Path what) = 0;
     virtual void umount() = 0;
 
-    virtual fs_status_code read(Path path, uint64_t count, uint64_t offset, char *buf) = 0;
-    virtual fs_status_code write(Path path, uint64_t count, uint64_t offset, char *buf) = 0;
+    virtual uint64_t read(Path path, uint64_t count, uint64_t offset, char *buf) = 0;
+    virtual uint64_t write(Path path, uint64_t count, uint64_t offset, char *buf) = 0;
 
-    virtual fs_status_code get_file_size(Path path, uint64_t *size) = 0;
-    virtual fs_status_code create_file(Path path) = 0;
-    virtual fs_status_code delete_file(Path path) = 0;
-    virtual fs_status_code list_files(Path path, Vector<Path> *) = 0;
+    virtual uint64_t get_file_size(Path path, uint64_t *size) = 0;
+    virtual uint64_t create_file(Path path) = 0;
+    virtual uint64_t delete_file(Path path) = 0;
+    virtual uint64_t list_files(Path path, Vector<Path> *) = 0;
 
     virtual bool file_exist(Path path) = 0;
 };
