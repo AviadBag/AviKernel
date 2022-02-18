af6a58911451d39ccf74033c2176101224f992f1
Aviad Bagno <aviad.bagno@gmail.com>
2022-02-18 14:59:48 +0200

    Created class FAT32File - a wrapper for the entries.

---
+++ include/fs/fat32/fat32_file.h
@@ -0,0 +1,39 @@
+#ifndef __FAT32_FILE_H__
+#define __FAT32_FILE_H__
+
+#include "fat32_directory_entry.h"
+#include <utils/vector.h>
+
+/* Represents a fat32 file. This is a higher level representation of a directory entry. */
+
+class FAT32File
+{
+public:
+    // A long entry is not necessary.
+    FAT32File(FAT32DirectoryEntry short_entry);
+    FAT32File(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries);
+
+    bool is_directory();
+    uint32_t get_size_bytes();
+    String get_name();
+
+private:
+    // The main method - converts the short and long entries into the fields of this class.
+    // <long_entries> can be empty
+    void setup_data(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries);
+
+    // Puts in <name> var the name of the file, extracted from the entiries. (long_entry is optional, like in setup_data)
+    void setup_name(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries);
+
+    // Puts in <name> the given 8.3 filename, converted to regular string.
+    void setup_name_8_3(String name83);
+
+    // Puts in <name> the long file name extracted from the given long entries.
+    void setup_name_long(Vector<FAT32DirectoryEntry> long_entries);
+
+    bool _is_directory;
+    uint32_t size_bytes;
+    String name;
+};
+
+#endif // __FAT32_FILE_H__
---
+++ src/fs/fat32/fat32_file.cpp
@@ -0,0 +1,83 @@
+#include "fs/fat32/fat32_file.h"
+
+#include <utils/vector.h>
+
+FAT32File::FAT32File(FAT32DirectoryEntry short_entry)
+{
+    Vector<FAT32DirectoryEntry> long_entries; // empty
+    setup_data(short_entry, long_entries);
+}
+
+FAT32File::FAT32File(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries)
+{
+    setup_data(short_entry, long_entries);
+}
+
+void FAT32File::setup_data(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries)
+{
+    _is_directory = short_entry.is_directory();
+    size_bytes = short_entry.se_get_file_size_bytes();
+    setup_name(short_entry, long_entries);
+}
+
+void FAT32File::setup_name(FAT32DirectoryEntry short_entry, Vector<FAT32DirectoryEntry> long_entries)
+{
+    if (long_entries.size() == 0) // No long entries
+        setup_name_8_3(short_entry.se_get_name());
+    else
+        setup_name_long(long_entries);
+}
+
+void FAT32File::setup_name_8_3(String name83)
+{
+    // Loop until we reach a space, or until we finish the 8 part.
+    for (int i = 0; i < 8; i++)
+    {
+        if (name83[i] == ' ') // This is the end of the 8 part.
+            break;
+        else
+            name += name83[i];
+    }
+
+    String extension;
+    // Until we finish the 3 part.
+    for (int i = 8; i < 11; i++)
+    {
+        if (name83[i] == ' ') // Spaces are for left padding, we should ignore them.
+            continue;
+        else
+            extension += name83[i];
+    }
+
+    if (!extension.empty())
+        name += '.' + extension;
+}
+
+// A helper function, that setup_name_long is giving to vector.sort in order to sort long entries by order.
+// (Returns true if first is bigger, as described in vector's docs)
+bool sort_long_entries(FAT32DirectoryEntry first, FAT32DirectoryEntry second)
+{
+    return first.le_get_order() > second.le_get_order();
+}
+
+void FAT32File::setup_name_long(Vector<FAT32DirectoryEntry> long_entries)
+{
+    long_entries.sort(sort_long_entries); // Sort by order
+    for (int i = 0; i < long_entries.size(); i++)
+        name += long_entries.get(i).le_get_name();
+}
+
+bool FAT32File::is_directory()
+{
+    return _is_directory;
+}
+
+uint32_t FAT32File::get_size_bytes()
+{
+    return size_bytes;
+}
+
+String FAT32File::get_name()
+{
+    return name;
+}
