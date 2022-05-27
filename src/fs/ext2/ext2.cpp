5c01595a847903449c318d83177584b54da60330
Aviad Bagno <aviad.bagno@gmail.com>
2022-05-27 12:34:24 +0300

    ext2.cpp - finished template

--- src/fs/ext2/ext2.cpp
+++ src/fs/ext2/ext2.cpp
@@ -1,1 +1,42 @@
 #include "fs/ext2/ext2.h"
+#include "fs/vfs/vfs.h"
+#include "fs/path.h"
+
+#include <cstdio.h>
+
+int Ext2::mount(Path what)
+{
+    printf("Mounting Ext2...");
+}
+
+int Ext2::umount()
+{
+}
+
+uint64_t Ext2::read(Path path, uint64_t count, uint64_t offset, char *buf)
+{
+}
+
+uint64_t Ext2::write(Path path, uint64_t count, uint64_t offset, char *buf)
+{
+}
+
+uint64_t Ext2::get_file_size(Path path, uint64_t *size)
+{
+}
+
+uint64_t Ext2::create_file(Path path)
+{
+}
+
+uint64_t Ext2::delete_file(Path path)
+{
+}
+
+uint64_t Ext2::list_files(Path path, Vector<Path> *)
+{
+}
+
+int Ext2::file_exist(Path path)
+{
+}
