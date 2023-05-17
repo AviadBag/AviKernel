c8b76b470b7a6b33437483d16a7cbdbbc7c7595c
Aviad <aviad.bagno@gmail.com>
2023-05-17 20:41:19 +0300

    Added more docs on virtual_mgr.h

--- include/kernel/mm/virtual_mgr/virtual_mgr.h
+++ include/kernel/mm/virtual_mgr/virtual_mgr.h
@@ -8,19 +8,20 @@
 
 #define VMMGR_PAGE_SIZE 4096
 
-typedef void* virtual_addr;
+typedef void *virtual_addr;
 
-class VirtualMgr {
+class VirtualMgr
+{
 public:
     /**
      * @brief Initializes the Virtual Memory Manager.
-     * 
+     *
      */
     static void initialize();
 
     /**
      * @brief Maps the given physical frame into the given virutal page.
-     * 
+     *
      * @param v_addr The page to map to. (MUST BE PAGE ALIGNED!)
      * @param p_addr The frame to map. (MUST BE PAGE ALIGNED!)
      * @param requires_supervisor Do the pages require a supervisor?
@@ -28,16 +29,24 @@ public:
     static void map(virtual_addr v_addr, physical_addr p_addr, bool requires_supervisor);
 
 private:
-    static uint32_t page_directory[1024] __attribute__((aligned(VMMGR_PAGE_SIZE)));
+    // For detailed information on page_etable see the corresponding avidocs file.
     static uint32_t page_etable[1024] __attribute__((aligned(VMMGR_PAGE_SIZE)));
+    static uint32_t page_directory[1024] __attribute__((aligned(VMMGR_PAGE_SIZE)));
 
-    // Converts a page table to it's actual address, given the index of the page table in the page directory.
+    /**
+     * @brief Gives the virtual address of the actual given page table.
+     *        Important - it does not give you the address that this page table is in charge of,
+     *        but where ACTUALLY the very page table is kept.
+     *
+     * @param index What page table do you want?
+     * @return virtual_addr
+     */
     static virtual_addr get_page_table_virtual_address(int index);
 
     // Maps the next <count> pages.
     /**
      * @brief Maps the next <count> pages.
-     * 
+     *
      * @param v_addr The virtual address of the page to start mapping to.
      * @param p_addr The physical address to start mapping from.
      * @param count  How many pages do you want to map?
@@ -47,9 +56,13 @@ private:
 
     /**
      * Puts the page_etable in the CURRENT page directory (pointed to by CR3).
-    */
+     */
     static void put_page_etable();
 
+    /**
+     * @brief Invalidates the cache for the given page.
+     *
+     */
     static inline void invalidate(virtual_addr);
 };
 
